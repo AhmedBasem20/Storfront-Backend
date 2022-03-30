e92323450112a4282a6f0e6a5521335f3a78eb4f
Ahmed Basem <62952819+AhmedBasem20@users.noreply.github.com>
2022-03-30 22:27:36 +0200

    Update product.ts

--- src/models/product.ts
+++ src/models/product.ts
@@ -20,4 +20,43 @@ export class productStore {
             throw new Error(`Could not get products. Error: ${err}`);
         }
     }
+    
+    async show(id:number): Promise<product[]> {
+        try {
+            const connect = await client.connect();
+        const sql = 'SELECT * FROM product WHERE id = (1)';
+        const result = await connect.query(sql, [id]);
+        connect.release();
+        return result.rows[0]; 
+        }
+        catch(err) {
+            throw new Error(`Could not show this product. Error: ${err}`);
+        }
+    }
+
+    async create(p: product): Promise<product[]> {
+        try {
+            const connect = await client.connect();
+        const sql = 'INSERT INTO products (name, price, category) VALUES(IPhone,10, mobiles) RETURNING *';
+        const result = await connect.query(sql, [p.name, p.price, p.category]);
+        connect.release();
+        return result.rows[0]; 
+        }
+        catch(err) {
+            throw new Error(`Could not create this product. Error: ${err}`);
+        }
+    }
+    
+    async delete(id:number): Promise<product[]> {
+        try {
+            const connect = await client.connect();
+        const sql = 'DELETE FROM products WHERE id=(1) RETURNING *';
+        const result = await connect.query(sql, [id]);
+        connect.release();
+        return result.rows[0]; 
+        }
+        catch(err) {
+            throw new Error(`Could not delete this product. Error: ${err}`);
+        }
+    }
 }
